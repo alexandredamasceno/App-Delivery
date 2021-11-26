6b56c31f5e198ed3624221f11a20068316da7b3e
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-11-26 15:36:37 -0300

    Feat: create SellerOrders page

---
+++ front-end/src/pages/SellerOrders.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SellerOrders() {
+  return (
+    <main>
+      <header>
+        <h1>Pedidos</h1>
+      </header>
+    </main>
+  );
+}
+
+export default SellerOrders;
